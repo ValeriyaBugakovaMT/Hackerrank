3ce58df024567e58601edb4e1341b34ef2d7bc40
ValeriyaBugakovaMT <lerabugakova@outlook.com>
2019-03-18 12:13:03 +0600

    сщььше1

---
+++ Part1.cs
@@ -0,0 +1,87 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+/*
+ * Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from  to  for three categories: problem clarity, originality, and difficulty.
+
+We define the rating for Alice's challenge to be the triplet , and the rating for Bob's challenge to be the triplet .
+
+Your task is to find their comparison points by comparing  with ,  with , and  with .
+
+If , then Alice is awarded  point.
+If , then Bob is awarded  point.
+If , then neither person receives a point.
+Comparison points is the total points a person earned.
+
+Given  and , determine their respective comparison points.
+
+For example,  and . For elements , Bob is awarded a point because . For the equal elements  and , no points are earned. Finally, for elements ,  so Alice receives a point. Your return array would be  with Alice's score first and Bob's second.
+
+Function Description
+
+Complete the function compareTriplets in the editor below. It must return an array of two integers, the first being Alice's score and the second being Bob's.
+
+compareTriplets has the following parameter(s):
+
+a: an array of integers representing Alice's challenge rating
+b: an array of integers representing Bob's challenge rating
+Input Format
+
+The first line contains  space-separated integers, , , and , describing the respective values in triplet . 
+The second line contains  space-separated integers, , , and , describing the respective values in triplet .
+
+Constraints
+
+Output Format
+
+Return an array of two integers denoting the respective comparison points earned by Alice and Bob.
+ * */
+
+class Solution
+{
+
+    // Complete the compareTriplets function below.
+    static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        int Alice = 0;
+        int Bob = 0;
+        for (int i = 0; i < a.Count(); i++)
+        {
+            Alice += a[i] > b[i] ? 1 : 0;
+            Bob += b[i] > a[i] ? 1 : 0;
+        }
+        List<int> result = new List<int> { };
+        result.Add(Alice);
+        result.Add(Bob);
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
