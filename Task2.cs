9c002618a1a4c6454afa81643c7d12db401b3e27
ValeriyaBugakovaMT <lerabugakova@outlook.com>
2019-03-18 13:06:49 +0600

    с

---
+++ Task2.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr)
+    {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+
+            a += arr[i][i];
+
+        }
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < arr[i].Length; j++)
+            {
+                if (i + j == arr.Length - 1)
+                {
+                    b += arr[i][j];
+                }
+            }
+        }
+        return Math.Abs(a - b);
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[][] arr = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
